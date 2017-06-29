2072b8b6bb5cb83d6d2002e7bcaaff2732d32984
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2017-06-29 13:19:25 +0200

    update copyright information

--- R/rxylib.R
+++ R/rxylib.R
@@ -5,7 +5,7 @@
 #' data from powder diffraction, spectroscopy and other experimental methods, like gamma-ray spectrometry.
 #'
 #' \tabular{ll}{ Package: \tab rxylib\cr Type: \tab Package\cr Version:
-#' \tab 0.1.0 \cr Date: \tab 2017-06-30 \cr License: \tab GPL-3 | LGPL-2.1 \cr }
+#' \tab 0.1.0 \cr Date: \tab 2017-06-30 \cr License: \tab GPL-3 | LGPL-2.1 (for the C++ library 'xylib') \cr }
 #'
 #' @details
 #'
@@ -16,7 +16,7 @@
 #' @name rxylib-package
 #' @aliases rxylib-package rxylib
 #' @docType package
-#' @author Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France), RLum Team (family support), Marcin Wojdyr (maintainer and author of the library `xylib`), Peng Zhang (author of the library `xylib`)
+#' @author Sebastian Kreutzer, IRAMAT-CRP2A, Universite Bordeaux Montaigne (France), RLum Team (family support), Marcin Wojdyr (maintainer and author of the C++ library `xylib`), Peng Zhang (author of the C++ library `xylib`)
 #'
 #' @keywords package
 #' @import utils
