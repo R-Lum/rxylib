d3761f12cb783ec368ac139c832180cf79daf1cc
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-11-24 00:43:32 +0100

    Switch to Makefile

--- CHECK_AND_BUILD_macOS_Linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-R -q -e "RLumBuild::build_package(
-  exclude = c(
-  'module_check_ReverseDependencies',
-  'module_add_RLumTeam'
-  ),
-  as_cran = TRUE,
-  write_Rbuildignore = TRUE
-)"
